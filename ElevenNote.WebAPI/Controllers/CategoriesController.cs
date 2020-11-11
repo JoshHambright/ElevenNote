d62f414a5dbdde8dacd984781729cc4a9ef65687
JoshHambright <josh.hambright@gmail.com>
2020-11-11 10:35:44 -0500

    category create, category service, category controller, dbset

---
+++ ElevenNote.WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,34 @@
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenNote.WebAPI.Controllers
+{
+    public class CategoriesController : ApiController
+    {
+        private CategoryService CreateCategoryService()
+        {
+            var useId = Guid.Parse(User.Identity.GetUserId());
+            var categoryService = new CategoryService(useId);
+            return categoryService;
+        }
+
+        //Create
+        public IHttpActionResult Post(CategoryCreate category)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateCategoryService();
+
+            if (!service.CreateCategory(category))
+                return InternalServerError();
+            return Ok();
+        }
+    }
+}
